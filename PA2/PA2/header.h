c0a253b9eb99a96f29e78110d7f91231ba8b61ae
minimouser <minimouser@gmail.com>
2019-01-30 12:35:11 -0800

    Program working, now needs some tweaking to fufil requirements

---
+++ PA2/PA2/header.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdio.h>
+#include <math.h>
+#define PI 3.141592
+
+//declare function prototypes
+void newtonSecond();
+void cylinderVolume();
+void charEncode();
+void distancePoints();
+void tangent();
+void resistance();
+void formula();
--- PA2/PA2/main.c
+++ PA2/PA2/main.c
@@ -5,3 +5,16 @@
 	Description: Same project as PA1 but this time using functions and multiple source files
 
 */
+
+#include "header.h"
+int main(void) {
+
+	//calls all the formulas
+	newtonSecond();
+	cylinderVolume();
+	charEncode();
+	distancePoints();
+	tangent();
+	resistance();
+	formula();
+}
